968cdec4c588f61aae8501f4da7dee936db4c6fd
mauricio <bedoyama@gmail.com>
2020-09-05 07:25:21 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Mauricio Bedoya on 9/5/20.
+//  Copyright © 2020 Mauricio Bedoya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
