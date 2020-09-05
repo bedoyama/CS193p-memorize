e1df2180b58f89bcc1ddd210c2741b79b29f5970
mauricio <bedoyama@gmail.com>
2020-09-05 08:40:01 -0500

    refactor CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,11 +12,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             ForEach(0..<4) { index in
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 12.0).stroke(lineWidth: 3)
-                    Text("👻")
-                }
+                CardView()
             }
         }
             .padding()
@@ -25,7 +21,15 @@ struct ContentView: View {
     }
 }
 
-
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 12.0).stroke(lineWidth: 3)
+            Text("👻")
+        }
+    }
+}
 
 
 
